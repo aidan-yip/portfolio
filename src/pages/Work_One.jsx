bb5e159f2b82defc409e377e3cb33eb7f5123a35
Aidan Y <jamesayipt6@gmail.com>
2025-03-27 14:10:10 -0700

    made api code cleaner and placed it in contentfulClient.js

--- src/pages/Work_One.jsx
+++ src/pages/Work_One.jsx
@@ -1,45 +1,7 @@
-import React, { useEffect, useState } from "react";
-
-// Contentful CMS
-import { client } from "../utilities/contentfulClient";
-import { CONTENTFUL_ENTRY_ID_1, WORK_IMAGE_1_ID } from "../globals/globals";
+import getWorkOne from "../utilities/contentfulClient";
 
 function Work_One() {
-  // text assets
-  const [workTitle1, setWorkTitle1] = useState("Loading...");
-  const [workDevInfo1, setWorkDevInfo1] = useState("Loading...");
-  const [workDesignInfo1, setWorkDesignInfo1] = useState("Loading...");
-  // image assets
-  const [workImage1, setWorkImage1] = useState(null);
-  const [workImage1Alt, setWorkImage1Alt] = useState("Loading...");
-
-  useEffect(() => {
-    const fetchData = () => {
-      client
-        .getEntry(CONTENTFUL_ENTRY_ID_1)
-        .then(({ fields }) => {
-          setWorkTitle1(fields.workTitle);
-          setWorkDevInfo1(fields.workDevInfo);
-          setWorkDesignInfo1(fields.workDesignInfo);
-          return client.getAsset(WORK_IMAGE_1_ID);
-        })
-        .then((asset) => {
-          setWorkImage1(asset.fields.file.url);
-          setWorkImage1Alt(asset.fields.description);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          setWorkTitle1("Error loading data");
-          setWorkDevInfo1("Error loading data");
-          setWorkDesignInfo1("Error loading data");
-          setWorkImage1Alt("Error loading data");
-        });
-    };
-    fetchData();
-  }, []);
-
-  console.log("workImage1", workImage1);
-  console.log("workImage1Alt", workImage1Alt);
+  const { workTitle1, workDevInfo1, workDesignInfo1, workImage1, workImage1Alt } = getWorkOne();
 
   return (
     <section className="section work_background">
--- src/utilities/contentfulClient.js
+++ src/utilities/contentfulClient.js
@@ -1,13 +1,51 @@
+// react
+import { useEffect, useState } from "react";
+
 // Contentful CMS
 import { createClient } from "contentful";
-import {
-  CONTENTFUL_TOKEN,
-  CONTENTFUL_SPACE,
-} from "../globals/globals";
+import { CONTENTFUL_TOKEN, CONTENTFUL_SPACE, CONTENTFUL_ENTRY_ID_1, WORK_IMAGE_1_ID  } from "../globals/globals";
 
 const client = createClient({
-  space: CONTENTFUL_SPACE, // Replace with your Space ID
-  accessToken: CONTENTFUL_TOKEN, // Replace with your Access Token
+  space: CONTENTFUL_SPACE, 
+  accessToken: CONTENTFUL_TOKEN,
 });
 
-export { client };
+// WORK ONE REST API
+function getWorkOne() {
+  // text assets
+  const [workTitle1, setWorkTitle1] = useState("Loading...");
+  const [workDevInfo1, setWorkDevInfo1] = useState("Loading...");
+  const [workDesignInfo1, setWorkDesignInfo1] = useState("Loading...");
+  // image assets
+  const [workImage1, setWorkImage1] = useState(null);
+  const [workImage1Alt, setWorkImage1Alt] = useState("Loading...");
+
+  useEffect(() => {
+    const fetchData = () => {
+      client
+        .getEntry(CONTENTFUL_ENTRY_ID_1)
+        .then(({ fields }) => {
+          setWorkTitle1(fields.workTitle);
+          setWorkDevInfo1(fields.workDevInfo);
+          setWorkDesignInfo1(fields.workDesignInfo);
+          return client.getAsset(WORK_IMAGE_1_ID);
+        })
+        .then((asset) => {
+          setWorkImage1(asset.fields.file.url);
+          setWorkImage1Alt(asset.fields.description);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          setWorkTitle1("Error loading data");
+          setWorkDevInfo1("Error loading data");
+          setWorkDesignInfo1("Error loading data");
+          setWorkImage1Alt("Error loading data");
+        });
+    };
+    fetchData();
+  }, []);
+
+  return { workTitle1, workDevInfo1, workDesignInfo1, workImage1, workImage1Alt };
+}
+
+export default getWorkOne;
